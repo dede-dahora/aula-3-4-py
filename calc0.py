62fae926768194d1719d7042b325830b2c4936b7
André Zauli <andresoares@colegiopaulodetarso.com.br>
2025-10-08 15:14:10 +0000

    commit

--- calc0.py
+++ calc0.py
@@ -7,3 +7,12 @@ print("A soma é: ", resultado)
 
 resultado = n1 * n2
 print("A multiplicação é: ", resultado)
+
+if n2 != 0:
+	resultado = n1 / n2
+	print("A divisão é: ", resultado)
+else:
+	print("Não é possível dividir por zero.")
+
+resultado = n1 - n2
+print("A subtração é: ", resultado)
