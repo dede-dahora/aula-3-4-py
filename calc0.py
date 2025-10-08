591839f0f8355008bc72b5e31ef0700462f4dd03
André Zauli <andresoares@colegiopaulodetarso.com.br>
2025-10-08 14:46:12 +0000

    commit

---
+++ calc0.py
@@ -0,0 +1,9 @@
+print("-----Calculadora-----")
+n1 = float(input("informe o primeiro número: "))
+n2 = float(input("informe o segundo número: "))
+
+resultado = n1 + n2
+print("A soma é: ", resultado)
+
+resultado = n1 * n2
+print("A multiplicação é: ", resultado)
